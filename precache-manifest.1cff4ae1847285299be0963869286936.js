a2f4259add7d314c7e82b5716229f8cd01ba0b56
ifjso <ojsboy@hotmail.com>
2022-10-04 14:47:21 +0900

    Updates

---
+++ precache-manifest.1cff4ae1847285299be0963869286936.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11389c7ad1bd5598c5b9cc44fdcfed59",
+    "url": "/rummikub-scoreboard-frontend/index.html"
+  },
+  {
+    "revision": "95acad27015936c6d8f4",
+    "url": "/rummikub-scoreboard-frontend/static/css/main.c11caca2.chunk.css"
+  },
+  {
+    "revision": "b7be7f2830b172414ce4",
+    "url": "/rummikub-scoreboard-frontend/static/js/2.90f9e6df.chunk.js"
+  },
+  {
+    "revision": "967c3aa677d48917bda81b3b2ff2693c",
+    "url": "/rummikub-scoreboard-frontend/static/js/2.90f9e6df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95acad27015936c6d8f4",
+    "url": "/rummikub-scoreboard-frontend/static/js/main.e42826af.chunk.js"
+  },
+  {
+    "revision": "e74209a348c0d7388e2f",
+    "url": "/rummikub-scoreboard-frontend/static/js/runtime-main.d31ea395.js"
+  },
+  {
+    "revision": "38825927627472491999d6c11369ad93",
+    "url": "/rummikub-scoreboard-frontend/static/media/histories_inactive.38825927.png"
+  },
+  {
+    "revision": "3334f4a6fdadd994cd75086b2d24a88e",
+    "url": "/rummikub-scoreboard-frontend/static/media/scoreboard_inactive.3334f4a6.png"
+  }
+]);
